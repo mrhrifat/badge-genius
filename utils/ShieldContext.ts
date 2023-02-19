851b70389f9555f665135b3ea4b727129ba856dd
mrhrifat <mrhrifat383759@gmail.com>
2023-02-19 23:48:34 +0600

    build: shield context api

---
+++ utils/ShieldContext.ts
@@ -0,0 +1,7 @@
+import { shieldContextType } from '@/type/utilsType'
+import React from 'react'
+import { shieldContextData } from './data'
+
+const SheildContext: shieldContextType = React.createContext(shieldContextData)
+
+export default SheildContext
