10b8b37026d826510ff0be60f96e7608c5674165
mrhrifat <mrhrifat383759@gmail.com>
2023-02-19 19:14:28 +0600

    build: metadata interface

---
+++ interfaces/utilsInterface.ts
@@ -0,0 +1,17 @@
+/**
+ * Title: utils
+ * Description: All utils interdace implement with Typescript
+ * Filename: utils.ts
+ * Path: /interfaces/utils.ts
+ * Author: Mrh Rifat (Programmer)
+ * Date: Feb 17, 2023
+ *
+ */
+
+export interface metadataInterface {
+  title: string
+  viewport: object
+  description: string
+  keywords: string[]
+  icons: object
+}
