d96969b572ff053fe9a3757068d810bf1d1fe9f9
mrhrifat <mrhrifat383759@gmail.com>
2023-02-19 19:15:33 +0600

    build: light & dark themes

---
+++ themes/theme.ts
@@ -0,0 +1,32 @@
+import { dosis, roboto_mono } from '@/utils/fonts'
+import { createTheme } from '@mui/material'
+
+export const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+    background: {
+      default: '#F8F9FA',
+    },
+    primary: {
+      main: '#22A6B3',
+    },
+  },
+  typography: {
+    fontFamily: [dosis.style.fontFamily, roboto_mono.style.fontFamily].join(''),
+  },
+})
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    background: {
+      default: '#121212',
+    },
+    primary: {
+      main: '#90CAF9',
+    },
+  },
+  typography: {
+    fontFamily: [dosis.style.fontFamily, roboto_mono.style.fontFamily].join(''),
+  },
+})
