f7abc7781a7376c07484493b8943de314ede530b
mrhrifat <mrhrifat383759@gmail.com>
2023-02-19 23:01:49 +0600

    build: font, metadata, mui icons & interface

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,37 @@
-import './globals.css'
+/**
+ * Title: layout
+ * Description: Root file to render
+ * Filename: layout.tsx
+ * Path: /app/layout.tsx
+ * Author: Mrh Rifat (Programmer)
+ * Date: Feb 17, 2023
+ *
+ */
 
+import { metadataInterface } from '@/interfaces/utilsInterface'
+import '@/styles/globals.css'
+import { metadata as Metadata } from '@/utils/data'
+import { dosis } from '@/utils/fonts'
+import Head from 'next/head'
+import React from 'react'
+
+// Metadata
+export const metadata: metadataInterface = Metadata
+
+// Root
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
+    <html lang="en" className={dosis.className}>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </Head>
       <body>{children}</body>
     </html>
   )
