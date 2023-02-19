4fb6bf1fd81b6f925567a0e0fe1b0d66d5d4dd99
mrhrifat <mrhrifat383759@gmail.com>
2023-02-19 19:17:44 +0600

    build: import font & build to use

---
+++ utils/fonts.ts
@@ -0,0 +1,24 @@
+/**
+ * Title: fonts
+ * Description:
+ * Filename: fonts.ts
+ * Path: /utils/fonts.ts
+ * Author: Mrh Rifat (Programmer)
+ * Date: Feb 17, 2023
+ *
+ */
+
+import { Dosis, Roboto } from '@next/font/google'
+
+// TODO: interface/type later
+
+export const dosis = Dosis({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const roboto = Roboto({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['300', '400', '700'],
+})
