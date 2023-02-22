172b811d7ce80d872b9982777b9770ffdb2fc809
mrhrifat <mrhrifat383759@gmail.com>
2023-02-22 21:39:29 +0600

    build: toggle key dynamic components

---
+++ components/dynamic/ToggleKey.tsx
@@ -0,0 +1,39 @@
+/**
+ * Title: ToggleKey
+ * Description:
+ * Filename: ToggleKey.tsx
+ * Path: /components/dynamic/ToggleKey.tsx
+ * Author: Mrh Rifat (Programmer)
+ * Date: Feb 22, 2023
+ *
+ */
+
+import { ToggleKeyOptionType, ToggleKeyType } from '@/types/componentsTypes'
+import ToggleButton from '@mui/material/ToggleButton'
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
+import { FC } from 'react'
+
+const ToggleKey: FC<ToggleKeyType> = ({
+  value,
+  handleChange,
+  options,
+}: ToggleKeyType) => {
+  return (
+    <ToggleButtonGroup
+      color="primary"
+      value={value}
+      exclusive
+      onChange={handleChange}
+      aria-label="Platform">
+      {options.map((option: ToggleKeyOptionType) => (
+        <ToggleButton
+          key={option.id}
+          value={option.value}
+          sx={{ padding: '5px 20px' }}>
+          {option.title}
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
+  )
+}
+export default ToggleKey
