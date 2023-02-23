c6ff6ab8234b040b52b3022c02ff23fac5ceadfd
mrhrifat <mrhrifat383759@gmail.com>
2023-02-23 17:04:04 +0600

    ci: comments

--- utils/ShieldContext.ts
+++ utils/ShieldContext.ts
@@ -1,12 +1,12 @@
 /**
-* Title: ShieldContext
-* Description:
-* Filename: ShieldContext.ts
-* Path: /utils/ShieldContext.ts
-* Author: Mrh Rifat (Programmer)
-* Date: Feb 22, 2023
-*
-*/
+ * Title: ShieldContext
+ * Description:
+ * Filename: ShieldContext.ts
+ * Path: /utils/ShieldContext.ts
+ * Author: Mrh Rifat (Programmer)
+ * Date: Feb 22, 2023
+ *
+ */
 
 import { ShieldContextValueType } from '@/types/utilsTypes'
 import { createContext } from 'react'
