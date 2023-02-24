1089edafe731d6f1727dabe470bd7ceefad22eb9
mrhrifat <mrhrifat383759@gmail.com>
2023-02-25 03:03:33 +0600

    build: shield provier & consuemer & all states

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,50 @@
+/**
+ * Title: page
+ * Description:
+ * Filename: page.tsx
+ * Path: /app/page.tsx
+ * Author: Mrh Rifat (Programmer)
+ * Date: Feb 22, 2023
+ *
+ */
+
 'use client'
 
 import Root from '@/components/Root'
-import { lightTheme } from '@/themes/theme'
+import { darkTheme, lightTheme } from '@/themes/theme'
+import { optionsdata } from '@/utils/data'
+import ShieldContext from '@/utils/ShieldContext'
 import { CssBaseline, ThemeProvider } from '@mui/material'
+import { useState } from 'react'
+import * as icons from 'simple-icons'
 
 export default function Home() {
+  const [theme, setTheme] = useState('light')
+  const [options, setOptions] = useState(optionsdata)
+  const [shield, setShield] = useState('')
+  const [selectedIcon, setSelectedIcon] = useState(icons.si1password)
+
   return (
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline />
-      <Root text="Page > Root" />
-    </ThemeProvider>
+    <ShieldContext.Provider
+      value={{
+        theme,
+        setTheme,
+        options,
+        setOptions,
+        shield,
+        setShield,
+        selectedIcon,
+        setSelectedIcon,
+        icons,
+      }}>
+      <ShieldContext.Consumer>
+        {() => (
+          <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+            <CssBaseline />
+            <Root />
+          </ThemeProvider>
+        )}
+      </ShieldContext.Consumer>
+    </ShieldContext.Provider>
   )
 }
