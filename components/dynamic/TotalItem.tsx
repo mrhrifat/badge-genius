df5b581d58ca45f3a913810a8c9ba73a218fbf27
mrhrifat <mrhrifat383759@gmail.com>
2023-02-25 03:05:26 +0600

    build: total item compoennt

---
+++ components/dynamic/TotalItem.tsx
@@ -0,0 +1,27 @@
+/**
+ * Title: TotalItem
+ * Description:
+ * Filename: TotalItem.tsx
+ * Path: /components/dynamic/TotalItem.tsx
+ * Author: Mrh Rifat (Programmer)
+ * Date: Feb 25, 2023
+ *
+ */
+
+import { optionRender } from '@/lib/utilsLib'
+import { TotalItemType } from '@/types/componentsTypes'
+import { Badge } from '@mui/material'
+
+const TotalItem = ({ value, icon }: TotalItemType) => {
+  return (
+    <Badge
+      badgeContent={optionRender(value).length}
+      showZero
+      color="primary"
+      max={5000}>
+      {icon}
+    </Badge>
+  )
+}
+
+export default TotalItem
