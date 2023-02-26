46e41eac04a20826093d3a43ed30b2b1d980bcc5
mrhrifat <mrhrifat383759@gmail.com>
2023-02-26 19:40:48 +0600

    build: uilts interfaces

--- interfaces/utilsInterfaces.ts
+++ interfaces/utilsInterfaces.ts
@@ -5,3 +5,22 @@ export interface OptionRenderInterface {
   // eslint-disable-next-line no-unused-vars
   (value: object | undefined): string[]
 }
+
+// Simple Icons
+export interface SimpleIconsInterface {
+  title: string
+  slug: string
+  hex: string
+  source: string
+  svg: string
+  path: string
+  guidelines: string
+  license?:
+    | string
+    | null
+    | {
+        type: string
+        url: string
+      }
+    | undefined
+}
