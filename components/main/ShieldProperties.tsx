8584e45f8944ae6aebd239088020dfc0fb1ec556
mrhrifat <mrhrifat383759@gmail.com>
2023-02-26 19:38:08 +0600

    build: shield properties

---
+++ components/main/ShieldProperties.tsx
@@ -0,0 +1,42 @@
+/**
+ * Title: ShieldProperties
+ * Description:
+ * Filename: ShieldProperties.tsx
+ * Path: /components/main/ShieldProperties.tsx
+ * Author: Mrh Rifat (Programmer)
+ * Date: Feb 25, 2023
+ *
+ */
+
+'use client'
+
+import ShieldContext from '@/utils/ShieldContext'
+import List from '@mui/material/List'
+import Stack from '@mui/material/Stack'
+import { useContext } from 'react'
+import { shieldLists } from '../../utils/data'
+import ShieldList from '../dynamic/ShieldList'
+
+const ShieldProperties = () => {
+  const shieldContextValue = useContext(ShieldContext)
+
+  return (
+    <Stack direction="column" gap={2}>
+      <List>
+        {shieldLists.map((shieldList) => (
+          <ShieldList
+            key={shieldList.id}
+            icon={shieldList.icon}
+            title={shieldList.title}
+            value={shieldList.value(
+              shieldContextValue?.options,
+              shieldList.title
+            )}
+          />
+        ))}
+      </List>
+    </Stack>
+  )
+}
+
+export default ShieldProperties
