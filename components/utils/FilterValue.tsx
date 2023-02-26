83efd6d78ffa1902c6a5687893df48c4728a3a83
mrhrifat <mrhrifat383759@gmail.com>
2023-02-26 19:38:58 +0600

    build: filter value

---
+++ components/utils/FilterValue.tsx
@@ -0,0 +1,23 @@
+import { regex } from '@/lib/utilsLib'
+import { Typography } from '@mui/material'
+import { FC } from 'react'
+
+export interface FilterValueInterface {
+  value: any
+}
+
+const FilterValue: FC<FilterValueInterface> = ({ value }) => {
+  if (regex.test(value) === true)
+    return (
+      <a
+        href={value}
+        target="_blank"
+        rel=" noopener noreferrer"
+        style={{ color: '#22a6b3' }}>
+        Visit Now
+      </a>
+    )
+  return <Typography variant="subtitle1">{value}</Typography>
+}
+
+export default FilterValue
