725116d62c543143d41f921462829efd3213c0bb
mrhrifat <mrhrifat383759@gmail.com>
2023-02-26 19:35:09 +0600

    build: all main component

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,11 +1,34 @@
-import { Grid } from '@mui/material'
-import SearchIcon from '../main/SearchIcon'
+/**
+ * Title: Home
+ * Description:
+ * Filename: Home.tsx
+ * Path: /components/home/Home.tsx
+ * Author: Mrh Rifat (Programmer)
+ * Date: Feb 25, 2023
+ *
+ */
 
-const Home = () => {
+import { Grid } from '@mui/material'
+import { FC } from 'react'
+import Operation from '../main/Operation'
+import SearchIcon from '../main/SearchIcon'
+import ShieldOptions from '../main/ShieldOptions'
+import ShieldProperties from '../main/ShieldProperties'
+
+const Home: FC = () => {
   return (
     <Grid container gap={1} justifyContent="space-between">
       <Grid item md={3} sm={5} xs={12}>
         <SearchIcon />
+        <ShieldOptions />
+      </Grid>
+
+      <Grid item md={5} sm={6} xs={12}>
+        <ShieldProperties />
+      </Grid>
+
+      <Grid item md={3} sm={12} xs={12}>
+        <Operation />
       </Grid>
     </Grid>
   )
