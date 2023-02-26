1b8c5e5c4a62851f7dc13deefb27bad8c4574174
mrhrifat <mrhrifat383759@gmail.com>
2023-02-26 19:37:04 +0600

    build: search icon

--- components/main/SearchIcon.tsx
+++ components/main/SearchIcon.tsx
@@ -1,36 +1,62 @@
-import { optionRender } from '@/lib/utilsLib'
+/**
+ * Title: SearchIcon
+ * Description:
+ * Filename: SearchIcon.tsx
+ * Path: /components/main/SearchIcon.tsx
+ * Author: Mrh Rifat (Programmer)
+ * Date: Feb 25, 2023
+ *
+ */
+
+'use client'
+
+import {
+  generateShield,
+  generateSimpleIcon,
+  optionRender,
+  updateLabelColor,
+} from '@/lib/utilsLib'
+import { HandleSelectIconHandlerType } from '@/types/componentsTypes'
+import { labelColors } from '@/utils/data'
 import ShieldContext from '@/utils/ShieldContext'
 import BadgeIcon from '@mui/icons-material/Badge'
 import { Autocomplete, TextField } from '@mui/material'
-import React, { useContext } from 'react'
+import React, { ChangeEvent, useContext } from 'react'
 import OptionAndIcon from '../dynamic/OptionAndIcon'
 import TotalItem from '../dynamic/TotalItem'
 import { default as ShiledListBox } from '../utils/ShiledListBox'
 
 const SearchIcon = () => {
   const shieldContextValue = useContext(ShieldContext)
-  console.log(typeof shieldContextValue?.icons)
 
-  //   const handleSelectIcon = (e) => {
-  //     const { title, hex, svg, license, guidelines, path, source, slug } =
-  //       generateSimpleIcon(e, icons)
+  const handleSelectIcon: HandleSelectIconHandlerType = (
+    event: ChangeEvent<object>,
+    value: string | null
+  ) => {
+    if (value) {
+      const icons = generateSimpleIcon(value, shieldContextValue?.icons)
+      console.log(icons)
 
-  //     setSelectedIcon(e)
-  //     setOptions({
-  //       ...options,
-  //       title,
-  //       svg,
-  //       hex,
-  //       license,
-  //       guidelines,
-  //       path,
-  //       source,
-  //       slug,
-  //       labelColor: hex,
-  //     })
-  //     updateLabelColor(labelColors, hex)
-  //     generateShield !== null ? setGenerateShield(null) : ''
-  //   }
+      const { title, hex, svg, license, guidelines, path, source, slug } = icons
+
+      shieldContextValue?.setSelectedIcon(value)
+
+      shieldContextValue?.setOptions({
+        ...shieldContextValue?.options,
+        title,
+        svg,
+        hex,
+        license,
+        guidelines,
+        path,
+        source,
+        slug,
+        labelColor: hex,
+      })
+      updateLabelColor(labelColors, hex)
+      generateShield !== null ? shieldContextValue?.setShield('') : ''
+    }
+  }
 
   return (
     <OptionAndIcon
@@ -41,6 +67,7 @@ const SearchIcon = () => {
             options={optionRender(shieldContextValue?.icons)}
             disableListWrap
             ListboxComponent={ShiledListBox}
+            onChange={(event, value) => handleSelectIcon(event, value)}
             renderInput={(params) => (
               <TextField {...params} label="Search Icon" />
             )}
