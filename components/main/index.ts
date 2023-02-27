72eceec520985f6b5594867734a984ab34813240
mrhrifat <mrhrifat383759@gmail.com>
2023-02-27 13:08:11 +0600

    build: import & export of components

---
+++ components/main/index.ts
@@ -0,0 +1,27 @@
+/**
+ * Title: index
+ * Description:
+ * Filename: index.ts
+ * Path: /components/main/index.ts
+ * Author: Mrh Rifat (Programmer)
+ * Date: Feb 27, 2023
+ *
+ */
+
+import Download from './Download'
+import Operation from './Operation'
+import SearchIcon from './SearchIcon'
+import ShieldMarkdown from './ShieldMarkdown'
+import ShieldOptions from './ShieldOptions'
+import ShieldProperties from './ShieldProperties'
+import ShieldSvg from './ShieldSVG'
+
+export {
+  Download,
+  Operation,
+  SearchIcon,
+  ShieldMarkdown,
+  ShieldOptions,
+  ShieldProperties,
+  ShieldSvg,
+}
