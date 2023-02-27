454aeeb151d3def244f8fa118dbbb86e8c6f839b
mrhrifat <mrhrifat383759@gmail.com>
2023-02-27 13:01:24 +0600

    build: import & export of dynamic components

---
+++ components/dynamic/index.ts
@@ -0,0 +1,21 @@
+import AlertMessage from './AlertMessage'
+import ColorPalette from './ColorPalette'
+import HeaderIcons from './HeaderIcons'
+import OptionAndIcon from './OptionAndIcon'
+import RenderSVG from './RenderSVG'
+import ShieldForm from './ShieldForm'
+import ShieldList from './ShieldList'
+import ToggleKey from './ToggleKey'
+import TotalItem from './TotalItem'
+
+export {
+  AlertMessage,
+  ColorPalette,
+  HeaderIcons,
+  OptionAndIcon,
+  ShieldForm,
+  ShieldList,
+  ToggleKey,
+  TotalItem,
+  RenderSVG,
+}
