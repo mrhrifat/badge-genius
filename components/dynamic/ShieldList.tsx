89a45f0fea1f5a3b355319e59cb8ef4a77187359
mrhrifat <mrhrifat383759@gmail.com>
2023-03-01 15:51:43 +0600

    perf: remove options building from objg

--- components/dynamic/ShieldList.tsx
+++ components/dynamic/ShieldList.tsx
@@ -8,21 +8,21 @@
  *
  */
 
-import { FilterValue } from '@/components/utils'
 import { ShielListInterace } from '@/interfaces/componentsInterfaces'
 import Divider from '@mui/material/Divider'
 import ListItem from '@mui/material/ListItem'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
 import { FC } from 'react'
+import { FilterValue } from '../utils'
 
-const ShieldList: FC<ShielListInterace> = ({ icon, title, value }) => {
+const ShieldList: FC<ShielListInterace> = ({ icon, title }) => {
   return (
     <>
       <ListItem disablePadding>
         <ListItemIcon>{icon}</ListItemIcon>
         <ListItemText primary={title} />
-        <FilterValue value={value} />
+        <FilterValue title={title} />
       </ListItem>
       <Divider />
     </>
