2dd2006653018b4ed7caf01814efb2a1d8d90b89
mrhrifat <mrhrifat383759@gmail.com>
2023-03-14 21:25:32 +0600

    build: render website & other value

--- components/utils/FilterValue.tsx
+++ components/utils/FilterValue.tsx
@@ -1,4 +1,4 @@
-import { regex, shieldListValue } from '@/lib/utilsLib'
+import { shieldListValue } from '@/lib/utilsLib'
 import ShieldContext from '@/utils/ShieldContext'
 import Typography from '@mui/material/Typography'
 import React, { FC, useContext } from 'react'
@@ -7,22 +7,22 @@ export interface FilterValueInterface {
   title: string
 }
 
+export const renderWebsite = (value: string) => (
+  <a
+    href={value}
+    target="_blank"
+    rel="noopener noreferrer"
+    style={{ color: '#22A6B3' }}>
+    Visit Now
+  </a>
+)
+
 const FilterValue: FC<FilterValueInterface> = ({ title }) => {
   const shieldContextValue = useContext(ShieldContext)
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const value: any = shieldListValue(shieldContextValue?.options, title)
 
-  if (regex.test(value) === true) {
-    return (
-      <a
-        href={value}
-        target="_blank"
-        rel=" noopener noreferrer"
-        style={{ color: '#22a6b3' }}>
-        Visit Now
-      </a>
-    )
-  }
   return <Typography variant="subtitle1">{value}</Typography>
 }
 
