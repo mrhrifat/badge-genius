cd99d7ffcba4678a49b7c3696f0171fdc13e65d0
mrhrifat <mrhrifat383759@gmail.com>
2023-03-14 22:48:35 +0600

    build: color palette of label & logo

--- components/dynamic/ShieldForm.tsx
+++ components/dynamic/ShieldForm.tsx
@@ -9,11 +9,34 @@
  */
 
 import { ShieldFormInterface } from '@/interfaces/componentsInterfaces'
+import CircleIcon from '@mui/icons-material/Circle'
 import FormControl from '@mui/material/FormControl'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import Select from '@mui/material/Select'
 import { FC } from 'react'
+
+const renderColorMenuItem = (title: string, item: string | number) => {
+  if (
+    title !== 'Shield Category' &&
+    title !== 'Shield Style' &&
+    title !== 'Shield Width' &&
+    title !== 'Logo Width'
+  ) {
+    return (
+      <CircleIcon
+        sx={{
+          color: `#${item}`,
+          height: 14,
+          width: 14,
+          border: '1px solid #22a6b3',
+          borderRadius: 50,
+          marginX: 0.5,
+        }}
+      />
+    )
+  }
+}
 
 const ShieldForm: FC<ShieldFormInterface> = ({
   title,
@@ -22,6 +45,8 @@ const ShieldForm: FC<ShieldFormInterface> = ({
   handleOptionsChange,
   menuProps,
 }) => {
+  
+
   return (
     <FormControl fullWidth>
       <InputLabel id={title}>{title}</InputLabel>
@@ -32,6 +57,8 @@ const ShieldForm: FC<ShieldFormInterface> = ({
         sx={{
           '& .MuiOutlinedInput-input': {
             padding: '11px',
+            display: 'flex',
+            alignItems: 'center',
           },
         }}
         value={optionsState}
@@ -39,8 +66,17 @@ const ShieldForm: FC<ShieldFormInterface> = ({
         onChange={(event) => handleOptionsChange(event)}
         MenuProps={menuProps}>
         {items?.map((item) => (
-          <MenuItem value={item} key={item}>
-            {item}
+          <MenuItem
+            value={item}
+            key={item}
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}>
+            <>
+              {item}
+              {renderColorMenuItem(title, item)}
+            </>
           </MenuItem>
         ))}
       </Select>
