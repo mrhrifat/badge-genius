382405b5e96129ac0df3069ed10bcfa4217d1787
mrhrifat <mrhrifat383759@gmail.com>
2023-03-19 10:30:29 +0600

    build: copy icon appearing

--- components/main/ShieldMarkdown.tsx
+++ components/main/ShieldMarkdown.tsx
@@ -8,43 +8,66 @@
  *
  */
 
-'use client'
-
 /* eslint-disable react/no-children-prop */
 import { AlertMessage } from '@/components/dynamic'
 import ShieldContext from '@/utils/ShieldContext'
-import List from '@mui/material/List'
-import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
-import { useContext } from 'react'
+import ContentCopySharpIcon from '@mui/icons-material/ContentCopySharp'
+import Card from '@mui/material/Card'
+import IconButton from '@mui/material/IconButton'
+import { useContext, useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 import useCopyToClipboard from '../hooks/useCopyToClipboard'
 
 const ShieldMarkdown = () => {
   const shieldContextValue = useContext(ShieldContext)
   const [isCopied, handleCopy] = useCopyToClipboard()
+  const [isHovered, setIsHovered] = useState(false)
+
   const markdown = `![](${shieldContextValue?.shield})`
 
   const handleClick = () => {
-    const text = 'Not ready yet'
-    handleCopy(
-      shieldContextValue?.shield === undefined
-        ? text
-        : shieldContextValue?.shield
-    )
+    if (shieldContextValue?.shield) {
+      handleCopy(shieldContextValue?.shield)
+    }
+  }
+
+  const handleMouseOver = () => {
+    setIsHovered(true)
+  }
+
+  const handleMouseLeave = () => {
+    setIsHovered(false)
   }
 
   return (
     <>
-      <List disablePadding>
-        <ListItem disablePadding>
-          <ListItemButton sx={{ padding: '0 1rem' }} onClick={handleClick}>
-            {shieldContextValue?.shield && (
-              <ReactMarkdown children={markdown} />
-            )}
-          </ListItemButton>
-        </ListItem>
-      </List>
+      <Card
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseLeave={handleMouseLeave}
+        sx={{
+          background: 'none',
+          boxShadow: 'none',
+          cursor: 'pointer',
+          opacity: isCopied ? 0.5 : isHovered ? 0.7 : 1, // Update the opacity based on state
+          // Add position relative to position copy icon
+          position: 'relative',
+        }}>
+        {shieldContextValue?.shield && <ReactMarkdown children={markdown} />}
+        {isHovered && !isCopied && (
+          // Render the copy icon when the card is hovered over
+          <IconButton
+            sx={{
+              position: 'absolute',
+              top: '17%',
+              right: '30%',
+              color: (theme) =>
+                theme.palette.mode === 'light' ? '#000' : '#FFF',
+            }}>
+            <ContentCopySharpIcon />
+          </IconButton>
+        )}
+      </Card>
 
       <AlertMessage isCopied={isCopied} />
     </>
