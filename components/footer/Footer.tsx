0198dabba3c8ff52ea8b020fcbf4fd23ed825868
mrhrifat <mrhrifat383759@gmail.com>
2023-03-20 21:35:03 +0600

    refactor: remove github icons

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -8,11 +8,8 @@
  *
  */
 
-import GitHubIcon from '@mui/icons-material/GitHub'
 import Box from '@mui/material/Box'
-import IconButton from '@mui/material/IconButton'
 import Stack from '@mui/material/Stack'
-import Tooltip from '@mui/material/Tooltip'
 import Typography from '@mui/material/Typography'
 
 const Footer = () => {
@@ -25,13 +22,6 @@ const Footer = () => {
         <Typography variant="subtitle1">
           Kindly support this projects by your donations
         </Typography>
-      </Box>
-      <Box>
-        <Tooltip title="GitHub">
-          <IconButton aria-label="github" color="primary">
-            <GitHubIcon />
-          </IconButton>
-        </Tooltip>
       </Box>
     </Stack>
   )
