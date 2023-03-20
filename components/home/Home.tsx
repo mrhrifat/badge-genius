d1e8fb5f2e5b42a5e034cdf88f50494ee6f9d91b
mrhrifat <mrhrifat383759@gmail.com>
2023-03-20 21:43:15 +0600

    refactor: update root layout

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -9,28 +9,31 @@
  */
 
 import {
+  GenerateAndReset,
   Operation,
   SearchIcon,
   ShieldOptions,
   ShieldProperties,
 } from '@/components/main'
 import { homeGridStyle } from '@/themes/theme'
-import { Grid } from '@mui/material'
+import { Divider, Grid } from '@mui/material'
 import { FC } from 'react'
 
 const Home: FC = () => {
   return (
-    <Grid container gap={1} justifyContent="space-between">
-      <Grid item md={4} sm={5} xs={12} sx={homeGridStyle}>
+    <Grid container gap={0.5} justifyContent="space-between">
+      <Grid item lg={4} md={3} sm={5} xs={12} sx={homeGridStyle}>
         <SearchIcon />
         <ShieldOptions />
       </Grid>
 
-      <Grid item md={4} sm={6} xs={12} sx={homeGridStyle}>
+      <Grid item lg={4} md={3} sm={6} xs={12} sx={homeGridStyle}>
         <ShieldProperties />
+        <Divider />
+        <GenerateAndReset />
       </Grid>
 
-      <Grid item md={3.5} sm={12} xs={12} sx={homeGridStyle}>
+      <Grid item lg={3.5} md={2.5} sm={12} xs={12} sx={homeGridStyle}>
         <Operation />
       </Grid>
     </Grid>
