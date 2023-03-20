400da007e8c1f159dcdf087b28f3bb9854e4af1a
mrhrifat <mrhrifat383759@gmail.com>
2023-03-20 22:27:55 +0600

    refactor: option & icon

---
+++ components/dynamic/OptionIconGrid.tsx
@@ -0,0 +1,27 @@
+/**
+ * Title: OptionAndIcon
+ * Description:
+ * Filename: OptionAndIcon.tsx
+ * Path: /components/dynamic/OptionAndIcon.tsx
+ * Author: Mrh Rifat (Programmer)
+ * Date: Feb 25, 2023
+ *
+ */
+
+import { OptionAndIconGridType } from '@/types/componentsTypes'
+import Grid from '@mui/material/Grid'
+
+const OptionIconGrid = ({ firstItem, secondItem }: OptionAndIconGridType) => {
+  return (
+    <Grid container gap={2}>
+      <Grid item xs={2} alignSelf="center" textAlign="center">
+        {firstItem}
+      </Grid>
+      <Grid item xs={8}>
+        {secondItem}
+      </Grid>
+    </Grid>
+  )
+}
+
+export default OptionIconGrid
