9d8e558d07f2d06ca8cf895979a682454a948482
mrhrifat <mrhrifat383759@gmail.com>
2023-03-26 01:12:55 +0600

    Revert "refactor: update layout"
    This reverts commit 50e852a8eed2365202aad14496cb101a75c07f51.

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -22,18 +22,18 @@ import { FC } from 'react'
 const Home: FC = () => {
   return (
     <Grid container gap={0.5} justifyContent="space-between">
-      <Grid item md={4} sm={6} xs={12} sx={homeGridStyle}>
+      <Grid item lg={4} md={3} sm={5} xs={12} sx={homeGridStyle}>
         <SearchIcon />
         <ShieldOptions />
       </Grid>
 
-      <Grid item md={4} sm={5.5} xs={12} sx={homeGridStyle}>
+      <Grid item lg={4} md={3} sm={6} xs={12} sx={homeGridStyle}>
         <ShieldProperties />
         <Divider />
         <GenerateAndReset />
       </Grid>
 
-      <Grid item md={3.5} sm={12} xs={12} sx={homeGridStyle}>
+      <Grid item lg={3.5} md={2.5} sm={12} xs={12} sx={homeGridStyle}>
         <Operation />
       </Grid>
     </Grid>
