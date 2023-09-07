ffa2e0900e83ad76c24a5d6cd93ca36f7cb2cf15
mrhrifat <mrhrifat383759@gmail.com>
2023-09-07 22:37:44 +0600

    refactor: update footer content

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -12,6 +12,7 @@ import { markdown } from '@/utils/data'
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
+import Link from 'next/link'
 import ReactMarkdown from 'react-markdown'
 
 const Footer = () => {
@@ -19,17 +20,23 @@ const Footer = () => {
     <Stack
       direction={{ sm: 'column', md: 'row' }}
       justifyContent="space-between"
-      alignItems={{ sm: 'center' }}
+      alignItems={{ xs: 'center' }}
       my={2}>
       <Box>
-        <Typography variant="subtitle1">
-          An Open Source Project, build & maintained by Rifat.
+        <Typography variant="subtitle1" data-testid="content1">
+          An Open Source Project, build & maintained by
+          <Link
+            href="https://github.com/mrhrifat"
+            target="_blank"
+            style={{ color: '#fff' }}>
+            Mrh Rifat
+          </Link>
         </Typography>
         <Typography variant="subtitle1">
           Kindly support this projects by your donations
         </Typography>
       </Box>
-      <ReactMarkdown linkTarget="_blank">{markdown}</ReactMarkdown>
+      <ReactMarkdown linkTarget="_blank" children={markdown} />
     </Stack>
   )
 }
